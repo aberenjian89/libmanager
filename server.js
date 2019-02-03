a532c0dcfbec280a5ac41b06ee4fb39e3da8ca3e
Ali Berenjian <ali.rberenjian@gmail.com>
2019-02-03 07:19:07 -0800

    Add Server Side

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const path = require("path");
+const http = require("http");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cookie_parser = require("cookie-parser");
+const session = require("express-session");
+
+require("dotenv").load();
+app = express();
+
+if (
+  process.env.ENVIRONMENT === "development" ||
+  process.env.ENVIRONMENT === "staging"
+) {
+  mongoose.connect("mongodb://localhost/libmanager_development", {
+    useNewUrlParser: true
+  });
+} else {
+  mongoose.connect("mongodb://localhost/libmanager_production", {
+    useNewUrlParser: true
+  });
+}
+
+let db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", function() {
+  console.log("Connected to local Database");
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookie_parser());
+app.use(
+  session({
+    secret: "fghdgdsfsf243D$32",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secret: true }
+  })
+);
+app.use(morgan("dev"));
+
+app.use(express.static(path.join(__dirname, "./dist/libmanager")));
+app.use(express.static(path.join(__dirname, "./dist/libmanager/assets")));
+
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./dist/libmanager/index.html"));
+});
+
+const port = process.env.PORT || "3000";
+
+const server = http.createServer(app);
+
+server.listen(port, "localhost", function() {
+  console.log(`Starting Server on Port ${port}`);
+});
